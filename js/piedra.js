0e643a5a306873927de692e4e03508f72c57b08a
Allan Aguilar <allanaguilar2@gmail.com>
2015-06-29 11:48:15 -0500

    agregado while

--- js/piedra.js
+++ js/piedra.js
@@ -16,8 +16,17 @@ var resultados =  [ ["Empate!",  "Perdise :(", "Ganaste!", "Ganaste!", "Perdiste
                     ["Perdise",  "Ganaste",    "Perdise",  "Empate",   "Ganaste"],
                     ["Ganaste",  "Perdise",    "Ganaste",  "Perdise",  "Empate"]];
 
-opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2\nLagarto: 3\nSpock: 4", 0);
-
-alert("Elegiste " + opciones[opcionUsuario]);
-alert("Javascript eligió " + opciones[opcionMaquina]);
-alert(resultados[opcionUsuario][opcionMaquina]);
+while(opcionUsuario != 5)
+{
+  opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2\nLagarto: 3\nSpock: 4\n\nSalir: 5", 0);
+  if (opcionUsuario == 5) 
+  {
+    alert("FIN DEL JUEGO ");
+  }
+  else
+  {
+    alert("Elegiste " + opciones[opcionUsuario]);
+    alert("Javascript eligió " + opciones[opcionMaquina]);
+    alert(resultados[opcionUsuario][opcionMaquina]);  
+  }
+}
